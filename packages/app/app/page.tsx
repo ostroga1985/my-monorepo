01e4145ee725c06de5d9a71956e0319c58f19c96
Anton Ostroga <you@example.com>
2026-02-05 19:08:42 +0300

    initial commit

---
+++ packages/app/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'ui';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>App</h1>
+      <Button />
+    </div>
+  );
+}
---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,3 @@
+// import Button from '@mui/material/Button';
+const Button = () => <div>stвфывas</div>;
+export { Button };
